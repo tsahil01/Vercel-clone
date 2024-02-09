35d5139984489c3cf055ad266cb1fe5059bfadcf
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-09 12:43:06 +0530

    files array to put in S#

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from "cors";
 import simpleGit from 'simple-git'
+import path from "path"
 import { generateRandomId } from './utils';
+import { getAllFiles } from './pathsAndFiles';
 
 const app = express();
 const PORT = 4000
@@ -9,16 +11,18 @@ const PORT = 4000
 app.use(cors())
 app.use(express.json())
 
-function generate(){
-
-}
-
 app.post('/deploy', async (req, res)=>{
     const repoURL = req.body.repoURL;
     console.log(repoURL)
     const randId = generateRandomId();
-    await simpleGit().clone(repoURL, `output/${randId}`)
-    res.json({})
+    await simpleGit().clone(repoURL, path.join(__dirname,`/output/${randId}`))
+
+    const files = getAllFiles(path.join(__dirname,`/output/${randId}`))
+
+    res.json({
+        msg: "Cloned",
+        id: randId // put it in S3
+    })
 })
 
 
